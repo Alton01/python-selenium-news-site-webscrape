dff00ef11bdbe6209b1b6abfdcb784179d394ff4
Altono1 <altonobinna11@gmail.com>
2025-07-29 20:47:10 +0100

    implement get_elements1

--- news-extract-data.py
+++ news-extract-data.py
@@ -1,16 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By
+import pandas as pd
 
 driver = webdriver.Chrome()
 driver.get("https://www.thesun.co.uk/sport/football/")
 
 
 # for getting multiple elements with same xpath class
-# getting both title and subtitle
-containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+containers = driver.find_elements(by=By.XPATH, value='//div[@class="teaser__copy-container"]')
 
-# getting title, subtitle and link individually
+titles = []
+subtitles = []
+links = []
+
+# itterating to get title, subtitle and link individually
 for container in containers:
-    title = container.find_element(by="xpath", value='./a/span').text
-    subtitle = container.find_element(by="xpath", value='./a/h3').text
-    link = container.find_element(by="xpath", value='./a').get_attribute("href")
+    title = container.find_element(by=By.XPATH, value='./a/span').text
+    subtitle = container.find_element(by=By.XPATH, value='./a/h3').text
+    link = container.find_element(by=By.XPATH, value='./a').get_attribute("href")
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+
+my_dict = {'title' : titles, 'subtitle' : subtitles, 'link' : links }
+
+df_headlines = pd.Dataframe(my_dict)
+
+# export dataframe in csv format
+df_headlines.to_csv('headline.csv')
+
+driver.quit()
+
