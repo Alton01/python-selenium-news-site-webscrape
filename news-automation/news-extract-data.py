67fa6e0c9e72b004e241660e92ed0481c243b904
Altono1 <altonobinna11@gmail.com>
2025-07-30 10:00:19 +0100

    fix Df typo/ generate csv

--- news-automation/news-extract-data.py
+++ news-automation/news-extract-data.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotVisibleException
 import pandas as pd
 
 driver = webdriver.Chrome()
@@ -8,7 +11,8 @@ driver.get("https://www.thesun.co.uk/sport/football/")
 
 
 # for getting multiple elements with same xpath class
-containers = driver.find_elements(by=By.XPATH, value='//div[@class="teaser__copy-container"]')
+containers = driver.find_elements(By.XPATH, value='//div[@class="teaser__copy-container"]')
+
 
 titles = []
 subtitles = []
@@ -16,9 +20,9 @@ links = []
 
 # itterating to get title, subtitle and link individually
 for container in containers:
-    title = container.find_element(by=By.XPATH, value='./a/span').text
-    subtitle = container.find_element(by=By.XPATH, value='./a/h3').text
-    link = container.find_element(by=By.XPATH, value='./a').get_attribute("href")
+    title = container.find_element(By.XPATH, value='//div[@class="teaser__copy-container"]/a/span').text
+    subtitle = container.find_element(By.XPATH, value='//div[@class="teaser__copy-container"]/a/h3').text
+    link = container.find_element(By.XPATH, value='//div[@class="teaser__copy-container"]/a').get_attribute("href")
     titles.append(title)
     subtitles.append(subtitle)
     links.append(link)
@@ -26,10 +30,9 @@ for container in containers:
 
 my_dict = {'title': titles, 'subtitle': subtitles, 'link': links }
 
-df_headlines = pd.Dataframe(my_dict)
+df_headlines = pd.DataFrame(my_dict)
 
 # export dataframe in csv format
 df_headlines.to_csv('headline.csv')
 
 driver.quit()
-
